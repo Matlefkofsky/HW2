bdd5ac7328e876c5d19d9c1ce29a2255cb23ee49
Mathew Lefkofsky <MatLefkofsky@Mathews-MacBook-Pro.local>
2017-09-26 14:34:53 -0400

    Finished

--- SI364-HW2.py
+++ SI364-HW2.py
@@ -10,17 +10,35 @@
 ## Edit the following Flask application code so that if you run the application locally and got to the URL http://localhost:5000/question, you see a form that asks you to enter your favorite number. Once you enter a number and submit it to the form, you should then see a web page that says "Double your favorite number is <number>". For example, if you enter 2 into the form, you should then see a page that says "Double your favorite number is 4". Careful about types in your Python code!
 ## You can assume a user will always enter a number only.
 
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/')
-def hello_to_you():
-    return 'Hello!'
+@app.route('/question')
+def doubleView():
+    html_form = '''
+    <html>
+    <body>
+    <form method="GET" action="http://localhost:5000/result-question">
+        <label>Enter your favorite number:
+            <input name="num" value="0" type="number">
+        </label></br>
+        <input type="submit" value="Submit">
+    </form>
+    </body>
+    </html>
+    '''
+    return html_form
+
+@app.route('/result-question', methods = ['GET', 'POST'])
+def result_double():
+    if request.method == 'GET':
+        result = request.args
+        number = int(result['num'])*2
+        return str("Double your favorite number is ") + str(number)
 
 
-if __name__ == '__main__':
-    app.run()
 
 
 ## [PROBLEM 2]
@@ -33,9 +51,33 @@ if __name__ == '__main__':
 # And use this opportunity to make sure you understand these steps: if you think going slowly and carefully writing out steps for a simpler data transaction, like Problem 1, will help build your understanding, you should definitely try that!
 
 # You can assume that a user will give you the type of input/response you expect in your form; you do not need to handle errors or user confusion. (e.g. if your form asks for a name, you can assume a user will type a reasonable name; if your form asks for a number, you can assume a user will type a reasonable number; if your form asks the user to select a checkbox, you can assume they will do that.)
+@app.route('/yourname')
+def nameBackwards():
+    html_form = '''
+    <html>
+    <body>
+    <form method="GET" action="http://localhost:5000/result-yourname">
+        <label>Enter your name:
+            <input name="yourName" value="" type="text">
+        </label></br>
+        <input type="submit" value="Submit">
+    </form>
+    </body>
+    </html>
+    '''
+    return html_form
 
+@app.route('/result-yourname', methods = ['GET', 'POST'])
+def result_nameBackwards():
+    if request.method == 'GET':
+        new_result = request.args
+        text = str(new_result['yourName'])
+        text_backwards = text[::-1]
+        text_lower = text_backwards.lower()
+        return str("your name backwards is ") + text_lower.title()
 
-
+if __name__ == '__main__':
+    app.run()
 
 
 
